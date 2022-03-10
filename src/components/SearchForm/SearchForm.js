036df342363fc76b2c5fc3cbb0d0e941fc102ac6
Harshit Raj <harshitr20@iitk.ac.in>
2022-03-10 23:51:34 +0530

    Add Fix serarch form
    Co-authored-by: Abhishek Shree <abhishek.shree@outlook.com>

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,46 +2,47 @@ import React from "react";
 
 import { FormGroup, Label, Button } from "reactstrap";
 import NotificationAlert from "react-notification-alert";
-import Select from 'react-select';
+import { useHistory } from "react-router-dom";
+import Select from "react-select";
 import { searchOptions } from "variables/companies";
 
 const customStyles = {
-  input:(provided) => ({
+  input: (provided) => ({
     ...provided,
-    color: '#1d8cf8',
+    color: "#1d8cf8",
   }),
-
 };
 
 const yearOptions = [
-  { value: 2022, label: '2022' },
-  { value: 2021, label: '2021' },
-  { value: 2020, label: '2020' },
-  { value: 2019, label: '2019' },
-]
+  { value: 2022, label: "2022" },
+  { value: 2021, label: "2021" },
+  { value: 2020, label: "2020" },
+  { value: 2019, label: "2019" },
+];
 
 const quarterOptions = [
   { value: "Q1", label: "Q1" },
   { value: "Q2", label: "Q2" },
   { value: "Q3", label: "Q3" },
   { value: "Q4", label: "Q4" },
-]
+];
 
 const SearchForm = () => {
-  const [search, setSearch] = React.useState({value:"", label:""});
-
+  const [search, setSearch] = React.useState({ value: "", label: "" });
   const [fromYear, setFromYear] = React.useState(yearOptions[1]);
   const [toYear, setToYear] = React.useState(yearOptions[0]);
   const [fromQuater, setFromQuater] = React.useState(quarterOptions[0]);
   const [toQuater, setToQuater] = React.useState(quarterOptions[0]);
-
   const notificationAlertRef = React.useRef(null);
+
+  const history = useHistory();
 
   const onSubmitButtonClick = (e) => {
     e.preventDefault();
     if (
       fromYear.value > toYear.value ||
-      (fromYear.value === toYear.value && parseInt(toQuater.value[1]) < parseInt(fromQuater.value[1]))
+      (fromYear.value === toYear.value &&
+        parseInt(toQuater.value[1]) < parseInt(fromQuater.value[1]))
     ) {
       let options = {};
       options = {
@@ -54,7 +55,7 @@ const SearchForm = () => {
       notificationAlertRef.current.notificationAlert(options);
       return;
     }
-    if(search.value.length === 0){
+    if (search.value.length === 0) {
       let options = {};
       options = {
         place: "tr",
@@ -66,7 +67,15 @@ const SearchForm = () => {
       notificationAlertRef.current.notificationAlert(options);
       return;
     }
-    console.log(search.value, fromYear.value, toYear.value, fromQuater.value, toQuater.value);
+    console.log(
+      search.value,
+      fromYear.value,
+      toYear.value,
+      fromQuater.value,
+      toQuater.value
+    );
+    // redirect to /tab
+    history.push("/tab");
   };
   return (
     <>
@@ -89,45 +98,45 @@ const SearchForm = () => {
           <FormGroup className="col-md-4">
             <Label for="fromYear">From Year</Label>
             <Select
-            styles={customStyles}
-            className="react-select info"
-            classNamePrefix="react-select"
-            value={fromYear}
-            onChange={(value) => setFromYear(value)}
-            options={yearOptions}
+              styles={customStyles}
+              className="react-select info"
+              classNamePrefix="react-select"
+              value={fromYear}
+              onChange={(value) => setFromYear(value)}
+              options={yearOptions}
             />
           </FormGroup>
           <FormGroup className="col-md-2">
             <Label for="fromQuater">From Quater</Label>
             <Select
-            styles={customStyles}
-            className="react-select info"
-            classNamePrefix="react-select"
-            value={fromQuater}
-            onChange={(value) => setFromQuater(value)}
-            options={quarterOptions}
+              styles={customStyles}
+              className="react-select info"
+              classNamePrefix="react-select"
+              value={fromQuater}
+              onChange={(value) => setFromQuater(value)}
+              options={quarterOptions}
             />
           </FormGroup>
           <FormGroup className="col-md-4">
             <Label for="toYear">To Year</Label>
             <Select
-            styles={customStyles}
-            className="react-select info"
-            classNamePrefix="react-select"
-            value={toYear}
-            onChange={(value) => setToYear(value)}
-            options={yearOptions}
+              styles={customStyles}
+              className="react-select info"
+              classNamePrefix="react-select"
+              value={toYear}
+              onChange={(value) => setToYear(value)}
+              options={yearOptions}
             />
           </FormGroup>
           <FormGroup className="col-md-2">
             <Label for="toQuater">To Quater</Label>
             <Select
-            styles={customStyles}
-            className="react-select info"
-            classNamePrefix="react-select"
-            value={toQuater}
-            onChange={(value) => setToQuater(value)}
-            options={quarterOptions}
+              styles={customStyles}
+              className="react-select info"
+              classNamePrefix="react-select"
+              value={toQuater}
+              onChange={(value) => setToQuater(value)}
+              options={quarterOptions}
             />
           </FormGroup>
         </div>
@@ -138,6 +147,5 @@ const SearchForm = () => {
     </>
   );
 };
-
 
 export default SearchForm;
