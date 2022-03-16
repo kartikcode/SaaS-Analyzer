2fa0aeec3505c325302cf1b83c733788c7a8a2a1
Abhishek Shree <abhi1234456@gmail.com>
2022-03-17 02:17:59 +0530

    Cleaned up some template code

--- src/index.js
+++ src/index.js
@@ -1,42 +1,19 @@
-/*!
-
-=========================================================
-* Black Dashboard PRO React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-pro-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import AuthLayout from "layouts/Auth/Auth.js";
-import AdminLayout from "layouts/Admin/Admin.js";
 import HomeLayout from "layouts/Home/Home.js";
-import RTLLayout from "layouts/RTL/RTL.js";
 import TabLayout from "layouts/Tab/Tab.js";
 import CompareLayout from "layouts/Compare/Compare.js";
 
 import "assets/css/nucleo-icons.css";
 import "react-notification-alert/dist/animate.css";
 import "assets/scss/black-dashboard-pro-react.scss?v=1.2.0";
-import "assets/demo/demo.css";
 import ExtractLayout from "layouts/Extract/Extract";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
-      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-      <Route path="/rtl" render={(props) => <RTLLayout {...props} />} />
       <Route path="/tab" render={(props) => <TabLayout {...props} />} />
       <Route path="/compare" render={(props) => <CompareLayout {...props} />} />
       <Route path="/extract" render={(props) => <ExtractLayout {...props} />} />
--- src/variables/icons.js
+++ src/variables/icons.js
@@ -1,19 +1,3 @@
-/*!
-
-=========================================================
-* Black Dashboard PRO React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-pro-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 const icons = [
   { name: "icon-alert-circle-exc" },
   { name: "icon-align-center" },
